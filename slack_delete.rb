179f1d95a8a224486fdbc0700712a85413f50375
James Martinez <jamescmartinez@gmail.com>
2016-03-15 08:51:13 -0700

    Shorten file expiration to 14 days ago

--- slack_delete.rb
+++ slack_delete.rb
@@ -5,7 +5,7 @@ require 'uri'
 @token = ENV['SLACK_TOKEN']
 
 def list_files
-  ts_to = (Time.now - 30 * 24 * 60 * 60).to_i # 30 days ago
+  ts_to = (Time.now - 14 * 24 * 60 * 60).to_i # 14 days ago
   params = {
     token: @token,
     ts_to: ts_to,
